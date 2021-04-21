70347fa15c8118bdf26ede25230108c3d4618a6f
Gautam <pbg2402@gmail.com>
2021-04-22 00:24:35 +0530

    Added: Navbar, Tailwind styles, Server Side rendered props

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home(props) {
+    console.log(props)
+    //client side rendering
     return (
         <div>
             <Head>
@@ -10,8 +15,25 @@ export default function Home() {
             </Head>
 
             <Header />
-            {/* Navbar component */}
-            {/* Results */}
+            <Nav />
+            <Results />
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    //this page only is server side rendered
+    //server render happens first
+    const genre = context.query.genre
+    const request = await fetch(
+        `https://api.themoviedb.org/3${
+            requests[genre]?.url || requests.fetchTrending.url
+        }`
+    ).then((res) => res.json())
+
+    return {
+        props: {
+            results: request.results,
+        },
+    }
 }
