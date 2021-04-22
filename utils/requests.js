d2c5076814eaab3f45f59e9b3a4dc9f0de2ee706
Gautam <pbg2402@gmail.com>
2021-04-22 12:27:36 +0530

    Added: API Routes

--- utils/requests.js
+++ utils/requests.js
@@ -35,4 +35,24 @@ export default {
         title: 'Mystery',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
     },
+
+    fetchSciFi: {
+        title: 'Sci-Fi',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    },
+
+    fetchWestern: {
+        title: 'Western',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    },
+
+    fetchAnimation: {
+        title: 'Animation',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+    },
+
+    fetchTV: {
+        title: 'TV Movie',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+    },
 }
